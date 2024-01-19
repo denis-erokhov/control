e497009e184717c736353ff228b44c2dc78b3752
Den\is <vip.erohov@mail.ru>
2024-01-19 10:40:13 +0400

    Решение готово

--- Program.cs
+++ Program.cs
@@ -25,3 +25,31 @@ foreach (string str in newArray)
     Console.WriteLine(str);
 }
 
+
+static string[] FilterArray(string[] inputArray)
+{
+int count = 0;
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] newArray = new string[count];
+
+int index = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        newArray[index] = inputArray[i];
+        index++;
+    }
+}
+
+return newArray;
+}
+
